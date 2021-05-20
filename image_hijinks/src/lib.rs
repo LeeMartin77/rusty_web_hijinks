0c03d41b4fa9cd83eb90d92b2ad856f7f47230a4
Lee Martin <lee.martin@pliminus.co.uk>
2021-05-20 19:33:49 +0000

    Update: basic dom manipulation

--- image_hijinks/src/lib.rs
+++ image_hijinks/src/lib.rs
@@ -1,11 +1,20 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
+#[wasm_bindgen(start)]
+pub fn img_generate() -> Result<(), JsValue> {
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("img")?;
+    val.set_id("image-holder");
+    val.set_attribute("src", "https://placekitten.com/200/300")?;
+
+    body.append_child(&val)?;
+
+    Ok(())
 }
