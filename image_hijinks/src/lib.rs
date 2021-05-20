552946c52edc805068c1950991bd6b8bb552fe35
Lee Martin <lee.martin@pliminus.co.uk>
2021-05-20 21:33:07 +0000

    WIP: Almost there, think the encoding is wrong in some way - the string is way too short

--- image_hijinks/src/lib.rs
+++ image_hijinks/src/lib.rs
@@ -1,7 +1,8 @@
+use image::GenericImage;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
-pub fn img_generate() -> Result<(), JsValue> {
+pub fn main() -> Result<(), JsValue> {
 
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
@@ -12,9 +13,37 @@ pub fn img_generate() -> Result<(), JsValue> {
     // Manufacture the element we're gonna append
     let val = document.create_element("img")?;
     val.set_id("image-holder");
-    val.set_attribute("src", "https://placekitten.com/200/300")?;
+
+    let image = generate_image();
+    let mut buf = Vec::new();
+    image.write_to(&mut buf, image::ImageOutputFormat::Png).unwrap();
+    let base64_string = base64::encode_config(buf, base64::URL_SAFE);
+
+    val.set_attribute("src", format!("data:image/png;base64,{}", base64_string).as_str())?;
 
     body.append_child(&val)?;
 
     Ok(())
+}
+
+//This is shamelessly stolen from the docs, I'm not this smart...
+pub fn generate_image() -> image::DynamicImage {
+    let imgx = 800;
+    let imgy = 800;
+
+    let mut dynimage = image::DynamicImage::new_rgb8(imgx, imgy);
+    let mut curx = 1;
+    let mut cury = 1;
+    while curx < imgx {
+        while cury < imgy {
+            let r = (0.3 * curx as f32) as u8;
+            let b = (0.3 * cury as f32) as u8;
+            dynimage.put_pixel(curx, cury, image::Rgba([r, 0, b, 255]));
+            cury = cury + 1;
+        }
+        curx = curx + 1;
+        cury = 1;
+    }
+
+    return dynimage;
 }
