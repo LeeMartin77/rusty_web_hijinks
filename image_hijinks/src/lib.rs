78e6dafb27f3a8b44ead4f9250d30dbe6c32ed3d
Lee Martin <lee.martin@pliminus.co.uk>
2021-05-21 22:28:44 +0000

    Gameloop that hangs forever, need to figure out interupting it to let the browser render...

--- image_hijinks/src/lib.rs
+++ image_hijinks/src/lib.rs
@@ -4,8 +4,16 @@ use image::GenericImage;
 
 use wasm_bindgen::prelude::*;
 
+pub struct GameProperties {
+    running: bool,
+    width: u32,
+    height: u32,
+    player_size: u32,
+    player_position: (u32, u32)
+}
+
 #[wasm_bindgen(start)]
-pub fn main() -> Result<(), JsValue> {
+pub fn main() {
 
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
@@ -13,43 +21,51 @@ pub fn main() -> Result<(), JsValue> {
     let document = window.document().expect("should have a document on window");
 
     let image_element = document.get_element_by_id("dynamic-image").expect("should have a dynamic-image image");
-    let imgx = 800;
-    let imgy = 800;
-    image_element.set_attribute("width", imgx.to_string().as_str()).expect("Unable to set dynamic image width");
-    image_element.set_attribute("height", imgy.to_string().as_str()).expect("Unable to set dynamic image height");
+    let mut game = GameProperties {
+        running: true,
+        width: 800,
+        height: 800,
+        player_size: 10,
+        player_position: (50, 50)
+    };
+    image_element.set_attribute("width", game.width.to_string().as_str()).expect("Unable to set dynamic image width");
+    image_element.set_attribute("height", game.height.to_string().as_str()).expect("Unable to set dynamic image height");
 
-    let image = generate_base_image(imgx, imgy);
+    let image = generate_image(&game);
 
-    write_dynimage_to_img(&image, &image_element);
-    
-    Ok(())
+    write_dynimage_to_img(image, &image_element);
+    while game.running {
+        game = game_loop(game);
+        let process_image = generate_image(&game);
+        write_dynimage_to_img(process_image, &image_element);
+    }
 }
 
-pub fn write_dynimage_to_img(dynimg: &DynamicImage, img_element: &Element) {
+pub fn game_loop(mut game: GameProperties) -> GameProperties {
+    game
+}
+
+pub fn write_dynimage_to_img(dynimg: DynamicImage, img_element: &Element) {
     let mut buf = Vec::new();
     dynimg.write_to(&mut buf, image::ImageOutputFormat::Png).expect("Failed to fill image buffer");
     let base64_string = base64::encode(buf);
     img_element.set_attribute("src", format!("data:image/png;base64,{}", base64_string).as_str()).expect("Unable to set dynamic image data");
 }
 
-pub fn generate_base_image(imgx: u32, imgy: u32) -> image::DynamicImage {
-    let mut dynimage = image::DynamicImage::new_rgb8(imgx, imgy);
+pub fn generate_image(game: &GameProperties) -> image::DynamicImage {
+    let mut dynimage = image::DynamicImage::new_rgb8(game.width, game.height);
     let mut curx = 1;
     let mut cury = 1;
 
-    let center_width = 20;
-    let center_height = 20;
-    let img_x_offset = imgx/2 - center_width/2;
-    let img_y_offset = imgy/2 - center_height/2;
+    let img_x_offset = game.player_position.0;
+    let img_y_offset = game.player_position.1;
 
-    while curx < imgx {
-        while cury < imgy {
-            let r = (0.3 * curx as f32) as u8;
-            let b = (0.3 * cury as f32) as u8;
-            if curx > img_x_offset && curx < img_x_offset + center_width && cury > img_y_offset && cury < img_y_offset + center_height {
-                dynimage.put_pixel(curx, cury, image::Rgba([0, 0, 0, 0]));
+    while curx < game.width {
+        while cury < game.height {
+            if curx > img_x_offset && curx < img_x_offset + game.player_size && cury > img_y_offset && cury < img_y_offset + game.player_size {
+                dynimage.put_pixel(curx, cury, image::Rgba([255, 0, 0, 255]));
             } else {
-                dynimage.put_pixel(curx, cury, image::Rgba([r, 0, b, 255]));
+                dynimage.put_pixel(curx, cury, image::Rgba([0, 0, 0, 255]));
             }
             cury = cury + 1;
         }
