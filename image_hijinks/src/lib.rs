2bb2a107087851ddd3fbdef4216e770f511c3244
Lee Martin <lee.martin@pliminus.co.uk>
2021-05-21 19:52:30 +0000

    Added: Adding a center block

--- image_hijinks/src/lib.rs
+++ image_hijinks/src/lib.rs
@@ -1,4 +1,5 @@
 use image::GenericImage;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
@@ -16,25 +17,34 @@ pub fn main() -> Result<(), JsValue> {
     image_element.set_attribute("width", imgx.to_string().as_str()).expect("Unable to set dynamic image width");
     image_element.set_attribute("height", imgy.to_string().as_str()).expect("Unable to set dynamic image height");
 
-    let image = generate_image(imgx, imgy);
+    let image = generate_base_image(imgx, imgy);
     let mut buf = Vec::new();
     image.write_to(&mut buf, image::ImageOutputFormat::Png).expect("Failed to fill image buffer");
     let base64_string = base64::encode(buf);
-
     image_element.set_attribute("src", format!("data:image/png;base64,{}", base64_string).as_str()).expect("Unable to set dynamic image data");
-
+    
     Ok(())
 }
 
-pub fn generate_image(imgx: u32, imgy: u32) -> image::DynamicImage {
+pub fn generate_base_image(imgx: u32, imgy: u32) -> image::DynamicImage {
     let mut dynimage = image::DynamicImage::new_rgb8(imgx, imgy);
     let mut curx = 1;
     let mut cury = 1;
+
+    let center_width = 20;
+    let center_height = 20;
+    let img_x_offset = imgx/2 - center_width/2;
+    let img_y_offset = imgy/2 - center_height/2;
+
     while curx < imgx {
         while cury < imgy {
             let r = (0.3 * curx as f32) as u8;
             let b = (0.3 * cury as f32) as u8;
-            dynimage.put_pixel(curx, cury, image::Rgba([r, 0, b, 255]));
+            if curx > img_x_offset && curx < img_x_offset + center_width && cury > img_y_offset && cury < img_y_offset + center_height {
+                dynimage.put_pixel(curx, cury, image::Rgba([0, 0, 0, 0]));
+            } else {
+                dynimage.put_pixel(curx, cury, image::Rgba([r, 0, b, 255]));
+            }
             cury = cury + 1;
         }
         curx = curx + 1;
